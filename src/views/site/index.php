beaf27d6ea821120bed665a417dcecd8163a84f1
borisey <slamsnv@gmail.com>
2024-12-19 21:28:06 +0300

    fix

--- src/views/site/index.php
+++ src/views/site/index.php
@@ -7,21 +7,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron text-center bg-transparent mt-5 mb-5">
-        <h1 class="display-4">Экологическое право</h1>
-
-        <?php
-
-        $bill = \app\models\Bill::find()
-            ->orderBy(['id' => SORT_DESC])
-            ->one();
-
-        echo '<pre>';
-        print_r($bill);
-        echo '<pre>';
-
-
-
-        ?>
+        <h1 class="display-4">123Экологическое право</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
