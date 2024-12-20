9998d1b2b0b5c066aa2d43a4cced3d3ecbe49cff
borisey <slamsnv@gmail.com>
2024-12-20 18:49:36 +0300

    fix

--- src/views/site/index.php
+++ src/views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron text-center bg-transparent mt-5 mb-5">
-        <h1 class="display-4">123Экологическое право</h1>
+        <h1 class="display-4">Экологическое право</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
